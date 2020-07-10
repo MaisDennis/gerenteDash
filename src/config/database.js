217db349eb68da7501952c7398435418f374c642
Dennis Lee <dennisdjlee@yahoo.com>
2020-07-10 13:49:04 -0300

    edit migrations: taskfeed.

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   // username: 'postgres',
   // password: 'docker',
   // database: 'postgres',
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
